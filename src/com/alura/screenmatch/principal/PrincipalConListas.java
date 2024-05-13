3a65f4ad145b01f53d6f7a99bfe875e377c4a514
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-05-13 11:11:05 -0500

    Actualizacion y avances del alula Tres

---
+++ src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,37 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+import com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalConListas {
+
+    public static void main(String[] args) {
+
+        var peliculaDeBruno = new Pelicula("El señor de los anillos",2001);
+        peliculaDeBruno.evalua(10);
+        Pelicula miPelicula = new Pelicula("Encanto",2021);
+        miPelicula.evalua(9);
+        Pelicula otraPelicula = new Pelicula("Avatar",2023);
+        otraPelicula.evalua(6);
+        Serie lost = new Serie("Lost",2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(peliculaDeBruno);
+        lista.add(miPelicula);
+        lista.add(otraPelicula);
+        lista.add(lost);
+
+        for (Titulo item: lista) {
+            System.out.println(item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 3) {
+                System.out.println(pelicula.getClasificacion());
+            }
+
+        }
+    }
+
+
+}
